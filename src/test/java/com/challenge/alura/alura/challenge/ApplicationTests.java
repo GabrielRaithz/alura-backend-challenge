0285094b3834778290c7b740847b2ecf361c8303
Gabriel Raithz <gabrielraithz@gmail.com>
2021-07-21 20:32:14 -0300

    Initial state coming from initializr

---
+++ src/test/java/com/challenge/alura/alura/challenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.alura.alura.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
